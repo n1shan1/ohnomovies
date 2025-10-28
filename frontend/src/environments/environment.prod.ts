2bf4ca3545bf2229c388830094ab7ff93892b61c
Nishant Dev <nishantdev03@gmail.com>
2025-10-28 19:57:19 +0530

    fix:deployment tf config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://98.89.180.29:4001/api/v1', // Direct backend URL for production deployment
+  // apiUrl: 'http://98.89.180.29:4001/api/v1', // Direct backend URL for production deployment,
+  apiUrl: 'http://98.91.3.100:4001/api/v1', // Spring Boot backend
 };
