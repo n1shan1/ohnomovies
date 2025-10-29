450309660fb06ea9456ade4f8f2766c1a068c272
Nishant Dev <nishantdev03@gmail.com>
2025-10-29 11:31:15 +0530

    minor changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://98.89.180.29:4001/api/v1', // Direct backend URL for production deployment,
-  apiUrl: 'http://98.91.3.100:4001/api/v1', // Spring Boot backend
+  // apiUrl: 'http://34.231.200.43:4001/api/v1', // Direct backend URL for production deployment,
+  apiUrl: 'http://34.231.200.43:4001/api/v1', // Spring Boot backend
 };
